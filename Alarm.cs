c8c65841003bbf3a5041ee2bad6535efc2c73f72
GurgenOhanyan <78640070+GurgenOhanyan@users.noreply.github.com>
2021-04-14 12:38:48 +0400

    Add files via upload

---
+++ Alarm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+namespace Alarm
+{
+    class Alarm
+    {
+        public string time { get; set;}
+
+        public void music()
+        {
+            while (true)
+            {
+                Console.Write("Breeen_Breeeen");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Input;
+namespace Alarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            Alarm alarm = new Alarm();
+            alarm.time = "07:00";
+            string time = Console.ReadLine();
+            Thread alarmThread = new Thread(alarm.music);
+            if (time == alarm.time)
+            {
+                alarmThread.Start();
+                while(Console.ReadKey(true).Key != ConsoleKey.Escape)
+                {
+
+                }
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    alarmThread.IsBackground = true;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
